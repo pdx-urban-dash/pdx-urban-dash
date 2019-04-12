68842ba2e73a86be8e27b8a0f398666eaaae8c73
Karis Sponsler <kariss@surveymonkey.com>
2019-04-11 18:06:27 -0700

    Fixing linting error.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
 import React from 'react';
-import LineChart from './components/viz/LineChart';
 
 /**
  * Root level App component
  */
-const App = () => (
-  <div className="App">
-    <LineChart
-      data={[
-        {
-          dataSetName: 'first',
-          description: 'this is first',
-          lineColor: 'red',
-          showTrendLine: true,
-          values: [
-            { x: 0, y: 10 },
-            { x: 1, y: 20 },
-            { x: 2, y: 15 },
-            { x: 3, y: 40 },
-          ],
-        },
-        {
-          dataSetName: 'second',
-          lineColor: 'blue',
-          description: 'this is second',
-          values: [
-            { x: 0, y: 20 },
-            { x: 1, y: 30 },
-            { x: 2, y: 19 },
-            { x: 3, y: 50 },
-          ],
-        },
-      ]}
-      //colorScheme="dark"
-    />
-  </div>
-);
+const App = () => (<div />);
 
 export default App;
