1ab62bd72d313f1875f8ca362cdf0e840846ce01
matthewgreenlaw <walneergwehttam@googlemail.com>
2019-04-11 17:10:18 -0700

    Update bar tooltips, cleanup app.js

--- src/App.js
+++ src/App.js
@@ -1,119 +1,37 @@
 import React from 'react';
-import BarChartStacked from './components/viz/BarCharts/BarChartStacked';
-import BarChartClustered from './components/viz/BarCharts/BarChartClustered';
-import BarChartGrouped from './components/viz/BarCharts/BarChartGrouped';
+import { BarChartStacked, BarChartClustered, BarChartGrouped } from './components/viz/BarCharts/BarCharts.js';
 
 /**
  * Root level App component
  */
+const testData = [
+  {
+    dataSetName: 'Total Crimes Against Persons',
+    description: 'Police Bureau, public safety',
+    values: [
+      { x: "January", y: 1 },
+      { x: "February", y: 20 },
+      { x: "March", y: 15 },
+      { x: "April", y: 40 },
+    ],
+  },
+  {
+    dataSetName: 'Total property offenses',
+    description: 'Police Bureau, public safety',
+    values: [
+      { x: "January", y: 3 },
+      { x: "February", y: 5 },
+      { x: "March", y: 2 },
+      { x: "April", y: 1 },
+    ],
+  },
+]
+
 const App = () => (
   <div className="App">
-    <BarChartClustered
-      data={[
-        {
-          dataSetName: 'first',
-          description: 'this is first',
-          values: [
-            { x: 0, y: 10 },
-            { x: 1, y: 20 },
-            { x: 2, y: 15 },
-            { x: 3, y: 40 },
-          ],
-        },
-        {
-          dataSetName: 'second',
-          description: 'this is second',
-          values: [
-            { x: 0, y: 20 },
-            { x: 1, y: 30 },
-            { x: 2, y: 19 },
-            { x: 3, y: 50 },
-          ],
-        },
-        {
-          dataSetName: 'third',
-          description: 'this is third',
-          values: [
-            { x: 0, y: 25 },
-            { x: 1, y: 33 },
-            { x: 2, y: 11 },
-            { x: 3, y: 40 },
-          ],
-        },
-      ]}
-      //colorScheme="dark"
-    />
-    <BarChartGrouped
-      data={[
-        {
-          dataSetName: 'first',
-          description: 'this is first',
-          values: [
-            { x: 0, y: 10 },
-            { x: 1, y: 20 },
-            { x: 2, y: 15 },
-            { x: 3, y: 40 },
-          ],
-        },
-        {
-          dataSetName: 'second',
-          description: 'this is second',
-          values: [
-            { x: 0, y: 20 },
-            { x: 1, y: 30 },
-            { x: 2, y: 19 },
-            { x: 3, y: 50 },
-          ],
-        },
-        {
-          dataSetName: 'third',
-          description: 'this is third',
-          values: [
-            { x: 0, y: 25 },
-            { x: 1, y: 33 },
-            { x: 2, y: 11 },
-            { x: 3, y: 40 },
-          ],
-        },
-      ]}
-      //colorScheme="dark"
-    />
-
-    <BarChartStacked
-      data={[
-        {
-          dataSetName: 'first',
-          description: 'this is first',
-          values: [
-            { x: 0, y: 10 },
-            { x: 1, y: 20 },
-            { x: 2, y: 15 },
-            { x: 3, y: 40 },
-          ],
-        },
-        {
-          dataSetName: 'second',
-          description: 'this is second',
-          values: [
-            { x: 0, y: 20 },
-            { x: 1, y: 30 },
-            { x: 2, y: 19 },
-            { x: 3, y: 50 },
-          ],
-        },
-        {
-          dataSetName: 'third',
-          description: 'this is third',
-          values: [
-            { x: 0, y: 25 },
-            { x: 1, y: 33 },
-            { x: 2, y: 11 },
-            { x: 3, y: 40 },
-          ],
-        },
-      ]}
-      //colorScheme="dark"
-    />
+    <BarChartClustered data={testData}/>
+    <BarChartGrouped data={testData}/>
+    <BarChartStacked data={testData}/>
   </div>
 );
 
