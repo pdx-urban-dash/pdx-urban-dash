2bf825de492d724a00c939b989b81e6afc8d2710
Mario Zavalas <mzavalas@pdx.edu>
2019-04-13 16:40:30 -0700

    Serve react app from express

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/test', (req, res) => res.send({ express: 'Hello World.' }));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
