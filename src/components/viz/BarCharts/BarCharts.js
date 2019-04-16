943789006c7d7cbba0b5979b860416217839e7c9
matthewgreenlaw <walneergwehttam@googlemail.com>
2019-04-16 10:28:38 -0700

    Add barcharts

---
+++ src/components/viz/BarCharts/BarCharts.js
@@ -0,0 +1,9 @@
+import BarChartStacked from './BarChartStacked';
+import BarChartClustered from './BarChartClustered';
+import BarChartGrouped from './BarChartGrouped';
+
+export {
+  BarChartStacked,
+  BarChartClustered,
+  BarChartGrouped,
+};
