f69096e9377c3aca25f92cf99f12df1d8101a982
Matthew Hilden <matt.hilden@gmail.com>
2019-04-18 16:03:39 -0700

    cleaned up conflicts

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import DonutChart from './components/viz/DonutChart';
->>>>>>> 43833c2510545533448b3592e9bdf129403948b3
 
 /**
  * Root level App component
  */
-<<<<<<< HEAD
 const App = () => (<div />);
-=======
-const App = () => (
-  <div className="App">
-    <DonutChart
-      title="title"
-      className=""
-      colorScheme="light"
-      data={[
-        {
-          dataSetName: 'first',
-          values: [
-            { id: 0, field: 10 },
-            { id: 1, field: 20 },
-            { id: 2, field: 15 },
-            { id: 3, field: 40 },
-            { id: 4, field: 90 },
-            { id: 5, field: 12 },
-            { id: 6, field: 35 },
-            { id: 7, field: 55 },
-          ],
-        },
-      ]}
-    />,
-  </div>
-);
->>>>>>> 43833c2510545533448b3592e9bdf129403948b3
 
 export default App;
