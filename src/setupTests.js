f62bf7247c94db367a4ee6af7fcf32829dced8e1
Matthew Hilden <matt.hilden@gmail.com>
2019-04-18 15:57:51 -0700

    finalized donut chart and donut chart percentage. added all tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import { configure } from 'enzyme';
-=======
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { configure } from 'enzyme';
-// eslint-disable-next-line import/no-extraneous-dependencies
->>>>>>> 43833c2510545533448b3592e9bdf129403948b3
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
