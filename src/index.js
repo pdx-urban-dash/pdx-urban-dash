d6b1f6a870750f277d75c306f48ea0604f6356a0
Karis Sponsler <kariss@surveymonkey.com>
2019-04-20 11:51:56 -0700

    Fixing linter error

--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import DonutChart from './components/viz/DonutChart';
 
 ReactDOM.render(
-  <DonutChart
-    data={[
-      {
-        name: 'first',
-        description: 'the first one',
-        value: 20,
-      },
-      {
-        name: 'second',
-        description: 'the second one',
-        value: 20,
-      },
-      {
-        name: 'third',
-        description: 'the third one',
-        value: 20,
-      },
-      {
-        name: 'fourth',
-        description: 'the fourth one',
-        value: 20,
-      },
-    ]}
-    title="My Chart Title"
-  />,
+  <App />,
   document.getElementById('root'),
 );
