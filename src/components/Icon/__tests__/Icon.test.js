63fdad899af8b387c60d2367ed9565a9281c1a4e
Ezra <efarrar@pdx.edu>
2019-04-30 13:56:58 -0700

    Added tests for icons

--- src/components/Icon/__tests__/Icon.test.js
+++ src/components/Icon/__tests__/Icon.test.js
@@ -9,3 +9,55 @@ describe('<Icon />', () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
+
+describe('<Icon />', () => {
+  test('Icon renders target', () => {
+    const wrapper = mount(<Icon type="off-target" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('<Icon />', () => {
+  test('Icon renders target', () => {
+    const wrapper = mount(<Icon type="on-target" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('<Icon />', () => {
+  test('Icon renders target', () => {
+    const wrapper = mount(<Icon type="TrendingUp" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('<Icon />', () => {
+  test('Icon renders target', () => {
+    const wrapper = mount(<Icon type="TrendingDown" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('<Icon />', () => {
+  test('Icon renders target', () => {
+    const wrapper = mount(<Icon type="Maximize" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('<Icon />', () => {
+  test('Icon renders target', () => {
+    const wrapper = mount(<Icon type="Minimize" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('<Icon />', () => {
+  test('Icon renders target', () => {
+    const wrapper = mount(<Icon type="Github" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+    
+    
+  
