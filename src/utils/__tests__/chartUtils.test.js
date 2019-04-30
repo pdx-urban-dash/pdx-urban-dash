ace1e86e2b0c0890171b493b70e28f437620b243
Caleb Boylan <calebboylan@gmail.com>
2019-04-29 17:29:54 -0700

    Add barchart generation

--- src/utils/__tests__/chartUtils.test.js
+++ src/utils/__tests__/chartUtils.test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import { genChart } from '../chartUtils';
 
 each([
-  [undefined, null, 0],
+  [undefined],
   [{
     target_trend: 'UP',
     target: 100,
@@ -18,7 +18,7 @@ each([
       {
         show_trendline: true,
         description: 'first datapoint',
-        title: 'bar1',
+        title: 'line1',
         color: 'RED',
         data_values: [
           [
@@ -31,13 +31,67 @@ each([
         metadata: '',
       },
     ],
-  }, 'marks', 1],
-]).test('genChart with obj: %o', (obj, expected, length) => {
+  }],
+  [{
+    metadata: 'Metadata placeholder',
+    title: 'bar1',
+    description: 'Dummy bar chart 1: 7 bars of random positive size.',
+    chart_type: 'BAR',
+    categories: [
+      'category 4',
+      'category 5',
+      'category 3',
+      'category 7',
+      'category 2',
+      'category 1',
+      'category 6',
+    ],
+    target: 30.0,
+    target_trend: 'STABLE',
+    axis_labels: [
+      [
+        'bar_labels',
+        'bar values',
+      ],
+    ],
+    data_sets: [
+      {
+        metadata: 'Metadata placeholder.',
+        title: "bar1 dataset member 1. Axis label(s): ['bar_labels', 'bar values']",
+        description: 'Contains bar1 data for type BAR chart.',
+        color: 'YELLOW',
+        show_trendline: true,
+        data_values: [
+          [
+            'bar 1',
+            'bar 2',
+            'bar 3',
+            'bar 4',
+            'bar 5',
+            'bar 6',
+            'bar 7',
+            'bar 8',
+          ],
+          [
+            78,
+            35,
+            72,
+            99,
+            33,
+            78,
+            75,
+            91,
+          ],
+        ],
+      },
+    ],
+  },
+  ]]).test('genChart with obj: %o', (obj) => {
   if (obj === undefined) {
     expect(genChart(obj)).toBeNull();
   } else {
-    const lineChart = genChart(obj, 'light');
-    const wrapper = shallow(<lineChart />);
+    const chart = <genChart chartJson={obj} colorScheme="light" />;
+    const wrapper = shallow(<chart />);
     expect(wrapper).toMatchSnapshot();
   }
 });
--- src/utils/chartUtils.js
+++ src/utils/chartUtils.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { colors } from '../constants';
 import LineChart from '../components/viz/LineChart';
+import { BarChartStacked, BarChartClustered, BarChartGrouped } from '../components/viz/BarCharts/BarCharts';
+import { DonutChart } from '../components/viz/DonutChart';
 
 // eslint-disable-next-line import /prefer-default-export
-export const genChart = (chartJson, colorScheme) => {
+export const genChart = (chartJson, colorScheme, className) => {
   if (chartJson === undefined) {
     return null;
   }
+  const fInput = { title: chartJson.title, colorScheme, className };
   if (chartJson.chart_type === 'LINE') {
     const data = [];
     chartJson.data_sets.forEach((chartData) => {
-      const values = []
-      chartData.data_values[0].map((e, i) => values.push({x: e, y: chartData.data_values[1][i]}));
+      const values = [];
+      chartData.data_values[0].map((e, i) => values.push({ x: e, y: chartData.data_values[1][i] }));
       console.log(values);
       data.push({
         dataSetName: chartData.title,
@@ -21,7 +24,31 @@ export const genChart = (chartJson, colorScheme) => {
         values,
       });
     });
-    const fInput = { title: chartJson.title, colorScheme, data };
+    fInput.data = data;
     return <LineChart {...fInput} />;
+  }
+  if (chartJson.chart_type.startsWith('BAR')) {
+    const data = [];
+    chartJson.data_sets.forEach((chartData) => {
+      const values = [];
+      chartData.data_values[0].map((e, i) => values.push({ x: e, y: chartData.data_values[1][i] }));
+      console.log(values);
+      data.push({
+        dataSetName: chartData.title,
+        barColor: chartData.color,
+        description: chartData.description,
+        values,
+      });
+    });
+    fInput.data = data;
+    if (chartJson.chart_type === 'BARCLUSTERED') {
+      return <BarChartClustered {...fInput} />;
+    }
+    if (chartJson.chart_type === 'BARSTACKED') {
+      return <BarChartStacked {...fInput} />;
+    }
+    if (chartJson.chart_type === 'BARGROUPED') {
+      return <BarChartGrouped {...fInput} />;
+    }
   }
 };
