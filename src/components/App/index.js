d9fb6c2da7873b4a7f633fcf41a5d41d11aa2bc8
Ezra <efarrar@pdx.edu>
2019-05-01 00:44:14 -0700

    Added reactstrap layout for current design

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,8 +8,6 @@ import {
   Jumbotron,
 } from 'reactstrap';
 import Navigation from './Navigation';
-import Article from './Article';
-import Filters from './Filters';
 
 export default class App extends Component {
   static propTypes = {
@@ -31,22 +29,49 @@ export default class App extends Component {
     const { filtersOpen } = this.state;
     return (
       <Fragment>
-        <Navigation title="Some Shit" />
+        <Navigation title="PDX Dashboard" />
         <Collapse isOpen={filtersOpen}>
-          <Jumbotron fluid>
-            <Container>
-              <Row>
-                <Col>
-                  <Article title="Tight tight tight">
-                    So fucking tight
-                  </Article>
-                </Col>
-                <Col>
-                  <Filters />
-                </Col>
-              </Row>
-            </Container>
-          </Jumbotron>
+          <Container>
+            <Row fluid>
+              <Col md="4">
+                <h1>Description</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </Col>
+              <Col md="4">
+                <h1>Filters</h1>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+              </Col>
+              <Col md="4">
+                <h1>Applied Filters</h1>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+              </Col>
+            </Row>
+          </Container>
+          <Row>
+            <Col style={{ padding: '1rem', 'text-align': 'center' }} lg="12">
+            Chart Area
+            </Col>
+          </Row>
         </Collapse>
       </Fragment>
     );
