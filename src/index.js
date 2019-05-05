d2a987ed1d6e023d0d2ad55ff2fa90beca2b7d79
Karis Sponsler <kariss@surveymonkey.com>
2019-05-05 08:46:49 -0700

    Adding Apollo GraphQL to server and client.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Icon from './components/Icon';
+import App from './components/App';
 
 ReactDOM.render(
   <div>
-    <Icon type="target" />
+    <App />
   </div>,
   document.getElementById('root'),
 );
