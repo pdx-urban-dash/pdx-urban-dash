af9c16efb599c83033172c2f078b11882543e098
Karis Sponsler <karikittie@users.noreply.github.com>
2019-05-07 17:45:14 -0700

    Adding Apollo GraphQL to server and client. (#73)

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const graphSchema = require('./src/graphAPI/schema');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+const graphQlServer = new ApolloServer({
+  typeDefs: graphSchema.typeDef,
+  resolvers: graphSchema.resolvers,
+});
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+graphQlServer.applyMiddleware({
+  app,
+  playground: {
+    endpoint: `http://localhost:${port}/graphql`,
+    settings: {
+      'editor.theme': 'light',
+    },
+  },
+});
 
 app.get('/test', (req, res) => res.send({ express: 'Hello World.' }));
 
