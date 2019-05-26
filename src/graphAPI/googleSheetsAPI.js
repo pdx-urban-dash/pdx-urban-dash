0e244f67e93177ae5be329c91b416429bc6fcbf4
Mario Zavalas <mzavalas@pdx.edu>
2019-05-26 15:21:53 -0700

    Fix eslint errors

--- src/graphAPI/googleSheetsAPI.js
+++ src/graphAPI/googleSheetsAPI.js
@@ -14,22 +14,69 @@ const credentials = require('./credentials');
 // const SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 const API_PATH = 'https://sheets.googleapis.com/v4/spreadsheets/';
 
+function genAuth() {
+  const {
+    client_secret,
+    client_id,
+    redirect_uris,
+  } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0],
+  );
+  oAuth2Client.setCredentials(token);
+  return oAuth2Client;
+}
 
-// TODO: grab all the sheets into an array
+function toColumn(num) {
+  let range = '';
+  while (num > 26) {
+    range = range.concat(String.fromCharCode(64 + (Math.floor(num / 26))));
+    num = (num % 26) + 1;
+  }
+  return range.concat(String.fromCharCode(64 + num));
+}
 
-// function authorize(callback) {
-//   const {
-//     client_secret,
-//     client_id,
-//     redirect_uris,
-//   } = credentials.installed;
-//   const oAuth2Client = new google.auth.OAuth2(
-//     client_id, client_secret, redirect_uris[0],
-//   );
-//   oAuth2Client.setCredentials(token);
-//   callback(oAuth2Client);
-// }
+function dataSetProcessor(chart, type) {
+  const attributeColumn = attr => ({
+    title: attr[0],
+    description: attr[1],
+    metaData: attr[2],
+    color: attr[3],
+    showTrendLine: attr[4],
+    values: [],
+  });
+  let data;
+  chart = R.transpose(chart).slice(1);
+  if (type === 'DONUT') {
+    data = attributeColumn(chart[0]);
+    data.values = [chart[1], chart[2], chart[3]];
+    return [data];      
+  } 
+  data = [];
+  return chart.reduce((accum, curr, i) => {
+    if (i % 3 === 0) {
+      accum.push(attributeColumn(curr));
+      return accum;
+    } 
+    accum[accum.length - 1].values.push(curr);
+    return accum;
+  }, data);
+}
 
+function valueProcessor(data) {
+  const chart = {
+    metaData: data.values[6][1],
+    title: data.values[3][1],
+    description: data.values[4][1],
+    type: data.values[0][0].split(' ')[0],
+    categories: data.values[11].slice(1), // remove first item
+    target: data.values[7][1],
+    targetTrend: data.values[8][1],
+  };
+  chart.axisLabels = chart.type === 'DONUT' ? [data.values[9][1]] : [data.values[9][1], data.values[10][1]];
+  chart.dataSets = dataSetProcessor(data.values.slice(14), chart.type);
+  return chart;
+}
 
 class GoogleSheetsAPI extends RESTDataSource {
   constructor() {
@@ -38,75 +85,11 @@ class GoogleSheetsAPI extends RESTDataSource {
     this.sheets = google.sheets('v4');
   }
 
-  genAuth() {
-    const {
-      client_secret,
-      client_id,
-      redirect_uris,
-    } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0],
-    );
-    oAuth2Client.setCredentials(token);
-    return oAuth2Client;
-  }
-
-  toColumn(num) {
-    let range = '';
-    while (num > 26) {
-      range = range.concat(String.fromCharCode(64 + (Math.floor(num / 26))));
-      num = (num % 26) + 1;
-    }
-    return range.concat(String.fromCharCode(64 + num));
-  }
-
-  valueProcessor(data) {
-    const chart = {
-      metaData: data.values[6][1],
-      title: data.values[3][1],
-      description: data.values[4][1],
-      type: data.values[0][0].split(' ')[0],
-      categories: data.values[11].slice(1), // remove first item
-      target: data.values[7][1],
-      targetTrend: data.values[8][1],
-    };
-    chart.axisLabels = chart.type === 'DONUT' ? [data.values[9][1]] : [data.values[9][1], data.values[10][1]];
-    chart.dataSets = this.dataSetProcessor(data.values.slice(14), chart.type);
-    return chart;
-  }
-    
-  dataSetProcessor(chart, type) {
-    const attributeColumn = attr => ({
-      title: attr[0],
-      description: attr[1],
-      metaData: attr[2],
-      color: attr[3],
-      showTrendLine: attr[4],
-      values: [],
-    });
-    let data;
-    chart = R.transpose(chart).slice(1);
-    if (type === 'DONUT') {
-      data = attributeColumn(chart[0]);
-      data.values = [chart[1], chart[2], chart[3]];
-      return [data];      
-    } 
-    data = [];
-    return chart.reduce((accum, curr, i) => {
-      if (i % 3 === 0) {
-        accum.push(attributeColumn(curr));
-        return accum;
-      } 
-      accum[accum.length - 1].values.push(curr);
-      return accum;
-    }, data);
-  }
-    
   async getChartTitles() {
     const retrieveData = new Promise(((resolve, reject) => {
       const request = {
         spreadSheetID: sheetID,
-        auth: this.genAuth(),
+        auth: genAuth(),
         range: [],
         includeGridData: false,
       };
@@ -134,10 +117,10 @@ class GoogleSheetsAPI extends RESTDataSource {
 
   async getCharts() {
     const titles = await this.getChartTitles();
-    const ranges = titles.map(chart => `${chart.title}!A1:${this.toColumn(chart.gridProperties.columnCount)}${chart.gridProperties.rowCount}`);
+    const ranges = titles.map(chart => `${chart.title}!A1:${toColumn(chart.gridProperties.columnCount)}${chart.gridProperties.rowCount}`);
     const request = {
       spreadSheetID: sheetID,
-      auth: this.genAuth(),
+      auth: genAuth(),
       ranges,
     };
     const retrieveData = new Promise((resolve, reject) => {
@@ -150,8 +133,7 @@ class GoogleSheetsAPI extends RESTDataSource {
       });
     });
     const results = await retrieveData;
-    return results.map(this.valueProcessor);
-    //   return this.get(`${sheetID}/values/`, param).then();
+    return results.map(valueProcessor);
   }
 }
 
